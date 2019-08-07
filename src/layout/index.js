2340b5763c7b5d913f8e856b95e584e5cb1ab518
Razaqul Tegar <myrazaqul@gmail.com>
2019-08-07 15:34:17 +0700

    Add favicon

--- src/layout/index.js
+++ src/layout/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Helmet from "react-helmet";
 import Navigation from "../components/Navigation";
 import config from "../utils/SiteConfig";
+import favicon from '../../static/logos/logo-48.png'
 import "../styles/main.scss";
 
 export default class MainLayout extends Component {
@@ -12,6 +13,7 @@ export default class MainLayout extends Component {
       <>
         <Helmet>
           <meta name="description" content={config.siteDescription} />
+          <link rel="shortcut icon" type="image/png" href={favicon} />
         </Helmet>
         <Navigation menuLinks={config.menuLinks} />
         <main>{children}</main>
