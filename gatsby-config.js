1e9f18eafdc3b8a9f25a57960b663f4263567d08
Razaqul Tegar <myrazaqul@gmail.com>
2019-08-09 07:53:30 +0700

    Update configuration for RSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
         query: `
         {
           site {
-            rssMetadata {
+            siteMetadata {
               title
               description
               siteUrl
