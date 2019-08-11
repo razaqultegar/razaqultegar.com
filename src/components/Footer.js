60682a4a885f00bca85a5cf5242058459300d10e
Razaqul Tegar <myrazaqul@gmail.com>
2019-08-12 06:51:40 +0700

    Update page index and footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-        <p>&copy; 2018 Razaqul Tegar | <a href="/rss.xml">RSS</a> • <a href={config.repo}>View Source</a></p>
+        <p>&copy; 2018 Razaqul Tegar | <a href="/rss.xml">RSS</a> • <a href={config.repo}>Kode sumber</a></p>
       </footer>
     );
   }
