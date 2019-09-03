c647fb88877f0e9f7738970d1602572ca1f8f743
Razaqul Tegar <myrazaqul@gmail.com>
2019-09-03 15:00:39 +0700

    Update image configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,7 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 850,
+              linkImagesToOriginal: false,
             },
           },
           'gatsby-remark-prismjs',
