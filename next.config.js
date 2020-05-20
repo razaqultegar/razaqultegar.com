81d8e54beb945cd44eb7717b7c15fa3a0b8d4213
razaqultegar <myrazaqul@gmail.com>
2020-05-21 02:30:15 +0700

    LOL, thats wrong a configure 🤣

--- next.config.js
+++ next.config.js
@@ -12,7 +12,19 @@ const withMDX = nextMDX({
 const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
 
 const nextConfig = {
-  pageExtensions: ['jsx', 'js', 'ts', 'tsx', 'mdx']
+  pageExtensions: ['jsx', 'js', 'ts', 'tsx', 'mdx'],
+  experimental: {
+    modern: true,
+    redirects() {
+      return [
+        {
+          source: '/solved-ireport-tidak-fungsi-di-mode-jar/',
+          permanent: true,
+          destination: '/article/solved-ireport-tidak-fungsi-di-mode-jar'
+        }
+      ]
+    }
+  }
 };
 
 module.exports = withMDX(withBundleAnalyzer(nextConfig));
