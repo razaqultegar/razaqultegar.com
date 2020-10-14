c7ae36a7136b5847c8cce5147fc1716878e8457c
razaqultegar <myrazaqul@gmail.com>
2020-10-14 21:42:58 +0700

    Revert to NextJS

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const readingTime = require('reading-time');
+const mdxPrism = require('mdx-prism');
+const withMdxEnhanced = require('next-mdx-enhanced');
+
+module.exports = withMdxEnhanced({
+  layoutPath: 'layouts',
+  defaultLayout: true,
+  remarkPlugins: [require('remark-slug')],
+  rehypePlugins: [mdxPrism],
+  extendFrontMatter: {
+    process: mdxContent => ({
+      wordCount: mdxContent.split(/\s+/gu).length,
+      readingTime: readingTime(mdxContent)
+    })
+  }
+})({
+  webpack: config => {
+    return config;
+  }
+});
